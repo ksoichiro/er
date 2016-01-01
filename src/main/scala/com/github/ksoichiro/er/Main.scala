9f734339061a59f516ae9d01cf22dd96ca8b9016
Soichiro Kashima <soichiro.kashima@gmail.com>
2016-01-01 23:28:12 +0900

    Refactor.

--- src/main/scala/com/github/ksoichiro/er/Main.scala
+++ src/main/scala/com/github/ksoichiro/er/Main.scala
@@ -5,11 +5,13 @@ import japgolly.scalajs.react.{Callback, ReactComponentB, ReactDOM}
 import org.scalajs.dom.document
 
 import scala.scalajs.js
+import scala.scalajs.js.annotation.JSExport
 import scalacss.Defaults._
 import scalacss.ScalaCssReact._
 
-object EntryPoint extends js.JSApp {
-  def main() = {
+object Main extends js.JSApp {
+  @JSExport
+  def main(): Unit = {
     AppStyles.addToDocument()
     val AddModelButton = ReactComponentB[Unit]("")
       .render(_ => <.button(^.onClick --> Callback.log("Add"), AppStyles.button, "Add model"))
