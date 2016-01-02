ce6c7e60af7c9379b08319a5d983660698cb27e4
Soichiro Kashima <soichiro.kashima@gmail.com>
2016-01-02 12:57:52 +0900

    Add test HTML and apply it to real content.
    Also, changed base CSS framework from PureCSS to Semantic UI.

--- src/main/scala/com/github/ksoichiro/er/AppStyles.scala
+++ src/main/scala/com/github/ksoichiro/er/AppStyles.scala
@@ -7,7 +7,34 @@ object AppStyles extends StyleSheet.Inline {
 
   import dsl._
 
+  val header = style(
+    backgroundColor(c"#009CFD"),
+    minHeight(48.px)
+  )
+
+  val menu = style(
+    minHeight(1000.px),
+    addClassNames("ui", "secondary", "vertical", "pointing", "menu"),
+    width(200.px)
+  )
+
+  val subMenu = style(
+    addClassNames("item")
+  )
+
+  val subMenuHeader = style(
+    addClassNames("header")
+  )
+
+  val subMenuMenu = style(
+    addClassNames("menu")
+  )
+
+  val subMenuMenuItem = style(
+    addClassNames("item")
+  )
+
   val button = style(
-    addClassNames("pure-button", "pure-button-primary")
+    addClassNames("ui", "button")
   )
 }
--- src/main/scala/com/github/ksoichiro/er/Main.scala
+++ src/main/scala/com/github/ksoichiro/er/Main.scala
@@ -1,7 +1,7 @@
 package com.github.ksoichiro.er
 
+import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
-import japgolly.scalajs.react.{Callback, ReactComponentB, ReactDOM}
 import org.scalajs.dom.document
 
 import scala.scalajs.js
@@ -13,9 +13,40 @@ object Main extends js.JSApp {
   @JSExport
   def main(): Unit = {
     AppStyles.addToDocument()
-    val AddModelButton = ReactComponentB[Unit]("")
-      .render(_ => <.button(^.onClick --> Callback.log("Add"), AppStyles.button, "Add model"))
+
+    val Header = ReactComponentB[Unit]("Header")
+      .render(_ =>
+      <.div(AppStyles.header))
       .buildU
-    ReactDOM.render(AddModelButton(), document.getElementById("content"))
+
+    val Menu = ReactComponentB[Unit]("Menu")
+      .render(_ =>
+      <.div(AppStyles.menu,
+        <.div(AppStyles.subMenu,
+          <.div(AppStyles.subMenuHeader, "Models"),
+          <.div(AppStyles.subMenuMenu,
+            <.a(AppStyles.subMenuMenuItem, "Company"),
+            <.a(AppStyles.subMenuMenuItem, "Department"),
+            <.a(AppStyles.subMenuMenuItem, "Employee"),
+            <.a(AppStyles.subMenuMenuItem, "+ Add new model")
+          )
+        ),
+        <.div(AppStyles.subMenu,
+          <.div(AppStyles.subMenuHeader, "Indexes"),
+          <.div(AppStyles.subMenuMenu,
+            <.a(AppStyles.subMenuMenuItem, "+ Add new index")
+          )
+        )
+      ))
+      .buildU
+
+    val Container = ReactComponentB[Unit]("Container")
+      .render(_ =>
+      <.div(
+        Header(),
+        Menu()))
+      .buildU
+
+    ReactDOM.render(Container(), document.getElementById("container"))
   }
 }
